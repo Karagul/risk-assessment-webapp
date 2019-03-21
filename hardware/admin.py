74c7b66b27548d0de6348012e9e4cc4abe12ada9
eezzo <derastafari@gmail.com>
2019-03-21 13:05:58 -0600

    Added application inline to hardware admin page

--- hardware/admin.py
+++ hardware/admin.py
@@ -3,14 +3,18 @@ from django.contrib import admin
 # Register your models here.
 from .models import NISTVendorOption, NISTOperatingSystemOption, NISTHardwareOption, Vendor, Hardware, OperatingSystem
 from .forms import HardwareForm, OperatingSystemForm
+from applications.models import Application
 
 admin.site.register(NISTVendorOption)
 admin.site.register(NISTOperatingSystemOption)
 admin.site.register(NISTHardwareOption)
+class ApplicationInline(admin.TabularInline):
+    model = Application.hardware.through
 
 class HardwareAdmin(admin.ModelAdmin):
     model = Hardware
     form = HardwareForm
+    inlines = [ApplicationInline]
 admin.site.register(Hardware, HardwareAdmin)
 
 class OperatingSystemAdmin(admin.ModelAdmin):
--- hardware/models.py
+++ hardware/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # NIST choicest models--------------------------------------------------------------------
 class NISTVendorOption(models.Model):
     name = models.CharField(max_length=255)
